4c52b583ca956916d05aa5796c829483c58233fe
Marc Bollon <marc@infocovery.com>
2016-09-27 18:12:16 -0500

    Initial Commit

---
+++ seque-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  seque-example
+//
+//  Created by Marc Bollon on 9/27/16.
+//  Copyright © 2016 Marc Bollon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
